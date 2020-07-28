7b9c6ca47de5749b86bb227ac827d4dfdd19803b
Isaiah Awotide <38901009+McZenith@users.noreply.github.com>
2020-07-28 20:18:33 +0100

    Update vm.js

--- dev/vm.js
+++ dev/vm.js
@@ -1,20 +1,17 @@
 const { Engine } = require("velocity");
 
 const velocityDataPrivate = require("./velocity.private.data.json");
-const VelocityDataProd = require("./velocity.data.prod.json");
+const velocityDataProd = require("./velocity.data.prod.json");
 const velocityDataDev = require("./velocity.data.json");
 
 let velocityData;
 
-if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "Production"
-) {
-  velocityData = VelocityDataProd;
+if (process.env.NODE_ENV === "Production") {
+  velocityData = velocityDataProd;
 } else {
   velocityData = velocityDataDev;
 }
-const engine = new Engine({ template: "./src/index.vm" });
+const engine = new Engine({ template: "./statics/index.vm" || "./src/index.vm" });
 
 module.exports = data => {
   return engine.render({
