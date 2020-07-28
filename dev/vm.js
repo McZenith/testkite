02bac1b56b66acd355ec8c8cedcef30945bbbd0f
Isaiah Awotide <awotideisaiah@yahoo.com>
2020-07-28 15:17:56 +0100

    Initial Commit

---
+++ dev/vm.js
@@ -0,0 +1,25 @@
+const { Engine } = require("velocity");
+
+const velocityDataPrivate = require("./velocity.private.data.json");
+const VelocityDataProd = require("./velocity.data.prod.json");
+const velocityDataDev = require("./velocity.data.json");
+
+let velocityData;
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "Production"
+) {
+  velocityData = VelocityDataProd;
+} else {
+  velocityData = velocityDataDev;
+}
+const engine = new Engine({ template: "./src/index.vm" });
+
+module.exports = data => {
+  return engine.render({
+    ...velocityData,
+    ...velocityDataPrivate,
+    ...data
+  });
+};
